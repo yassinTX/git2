bed0ac1bd4eae8691c71bcc395a4eeaa2e7ae818
yassinTX <yassin.diaa26@gmail.com>
2026-08-02 22:14:18 +0300

    Update comingsoon.js

--- comingsoon.js
+++ comingsoon.js
@@ -3,21 +3,6 @@
 ==========================================*/
 
 // ============================
-// LOADER
-// ============================
-
-window.addEventListener("load", () => {
-
-    const loader = document.getElementById("loader");
-
-    setTimeout(() => {
-        loader.style.opacity = "0";
-        loader.style.visibility = "hidden";
-    }, 900);
-
-});
-
-// ============================
 // MOBILE MENU
 // ============================
 
